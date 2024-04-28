f984fe6ca961da2372497f17aac2046521409202
Dmytro Pedan <dmitrijpedan84@gmail.com>
2024-04-28 13:17:40 +0200

    feature: changed build directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "./build",
+  },
   plugins: [react()],
   css: {
     postcss: {
