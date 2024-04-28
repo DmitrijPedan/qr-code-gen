b30992d2f976851337b83d8f962ea1769169553a
Dmytro Pedan <dmitrijpedan84@gmail.com>
2024-04-28 12:42:50 +0200

    feature: added download functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./index.css";
 import QRCode from "react-qr-code";
-import { useState, useEffect } from "react";
+import { useState, useRef } from "react";
+import { toPng } from "html-to-image";
 import {
   QrCodeIcon,
   PlusIcon,
@@ -9,14 +10,40 @@ import {
 } from "@heroicons/react/16/solid";
 import classNames from "classnames";
 
+type FormValues = {
+  value: string;
+  label: string;
+};
+
+const initialValues = {
+  value: "",
+  label: "",
+};
+
 function App() {
-  const [value, setValue] = useState<string>("");
-  const [inputValue, setInputValue] = useState<string>("");
-  const [label, setLabel] = useState<string>("");
+  const qrCodeRef = useRef<HTMLDivElement>(null);
+
+  const [formValues, setFormValues] = useState<FormValues>({
+    ...initialValues,
+  });
+  const [qrValues, setQrValues] = useState<FormValues>({ ...initialValues });
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setValue(inputValue);
+    setQrValues(formValues);
+  };
+
+  const toPngImage = async () => {
+    if (!qrCodeRef.current) return;
+    try {
+      const dataUrl = await toPng(qrCodeRef.current);
+      const a = document.createElement("a");
+      a.href = dataUrl;
+      a.download = "qr-code.png";
+      a.click();
+    } catch (error) {
+      console.error("oops, something went wrong!", error);
+    }
   };
 
   return (
@@ -33,23 +60,28 @@ function App() {
       </header>
       <div className="container px-2 py-8 mx-auto flex flex-col justify-center items-center">
         <div className="flex flex-col space-y-4 justify-center mb-12">
-          <div className="flex flex-col items-center justify-center  border p-2 sm:p-3 md:p-4 rounded-lg bg-white">
+          <div
+            ref={qrCodeRef}
+            className="flex flex-col items-center justify-center  border p-2 sm:p-3 md:p-4 rounded-lg bg-white"
+          >
             <div className="w-52 flex justify-center items-center sm:w-64 h-52 sm:h-64">
-              {value ? (
-                <QRCode value={value} className="w-full h-full" />
+              {qrValues.value ? (
+                <QRCode value={qrValues.value} className="w-full h-full" />
               ) : (
                 <p className="text-sm text-slate-600 text-center">
                   Please, provide your value in the form below
                 </p>
               )}
             </div>
-            {label && <p className="mt-4">{label}</p>}
+            {qrValues.label && (
+              <p className="mt-4 text-slate-900 font-bold">{qrValues.label}</p>
+            )}
           </div>
           <div className="text-slate-400 text-sm">
             <p>Your QR code will open this value:</p>
             <p className="italic">
-              {value ? (
-                <span className="text-white">"{value}"</span>
+              {qrValues.value ? (
+                <span className="text-white">"{qrValues.value}"</span>
               ) : (
                 <span className="">not provided</span>
               )}
@@ -57,10 +89,11 @@ function App() {
           </div>
           <div className="text-center">
             <button
+              onClick={toPngImage}
               className={classNames(
                 "p-2 bg-green-500 text-white disabled:bg-slate-600 disabled:cursor-not-allowed"
               )}
-              disabled={!value}
+              disabled={!qrValues.value}
             >
               <ArrowDownTrayIcon className="h-6 w-6 inline-block mr-2" />
               <span>Download PNG</span>
@@ -80,8 +113,8 @@ function App() {
                   type="button"
                   className="absolute top-1/2 right-1 -translate-y-1/2"
                   onClick={() => {
-                    setInputValue("");
-                    setValue("");
+                    setFormValues({ ...initialValues });
+                    setQrValues({ ...initialValues });
                   }}
                 >
                   <BackspaceIcon className="h-6 w-6 text-gray-200 hover:text-orange-500 transition-colors" />
@@ -89,19 +122,47 @@ function App() {
                 <input
                   id="qrValue"
                   type="text"
-                  value={inputValue}
+                  value={formValues.value}
                   className="py-2 px-3 w-full pr-10 text-slate-900"
                   placeholder="Enter your value here..."
                   onChange={(e) => {
-                    setInputValue(e.target.value);
+                    setFormValues({ ...formValues, value: e.target.value });
                   }}
+                />
+              </div>
+            </div>
+            <div>
+              <label className="block" htmlFor="qrLabel">
+                Label (optional):
+              </label>
+              <div className="relative mb-2">
+                <button
+                  title="Clear input"
+                  type="button"
+                  className="absolute top-1/2 right-1 -translate-y-1/2"
+                  onClick={() => {
+                    setFormValues({ ...formValues, label: "" });
+                    setQrValues({ ...qrValues, label: "" });
+                  }}
+                >
+                  <BackspaceIcon className="h-6 w-6 text-gray-200 hover:text-orange-500 transition-colors" />
+                </button>
+                <input
+                  id="qrLabel"
+                  type="text"
+                  value={formValues.label}
+                  className="py-2 px-3 w-full pr-10 text-slate-900"
+                  placeholder="Enter your value here..."
+                  onChange={(e) =>
+                    setFormValues({ ...formValues, label: e.target.value })
+                  }
                 />
               </div>
             </div>
             <button
               type="submit"
               className="text-center mt-2 py-1 px-4 bg-green-500 text-white disabled:bg-gray-300 disabled:cursor-not-allowed"
-              disabled={!inputValue}
+              disabled={!formValues.value}
             >
               <span>
                 <PlusIcon className="h-4 w-4 mr-1 inline-block" />
