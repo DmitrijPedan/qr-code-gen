95fae15eb665ff226d3abdc2b732aee9313c9fef
Dmytro Pedan <dmitrijpedan84@gmail.com>
2024-04-28 10:42:15 +0200

    feature: added qr-code lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import "./index.css";
+import QRCode from "react-qr-code";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [value, setValue] = useState<string>("");
+  const [inputValue, setInputValue] = useState<string>("");
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">QR code gen</h1>
+    <div className="border">
+      <h1 className="text-3xl font-bold">QR code gen</h1>
+      <QRCode value={value} />
+      <p>{value}</p>
+      <input
+        type="text"
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          setValue(inputValue);
+        }}
+      >
+        Generate
+      </button>
     </div>
   );
 }
